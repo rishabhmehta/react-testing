bf0f8f4104ce754736d6a0bf7aec9824459cee34
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-23 02:08:26 +0530

    test driven development

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import { Greet } from './Greet';
+import { render, screen } from "@testing-library/react"
+import { Greet } from "./Greet"
 
-test('Greet renders correctly', () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+test("Greet renders correctly", () => {
+  render(<Greet />)
+  const textElement = screen.getByText("Hello")
+  expect(textElement).toBeInTheDocument()
+})
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Rishabh" />)
+  const textElement = screen.getByText("Hello Rishabh")
+  expect(textElement).toBeInTheDocument()
+})
