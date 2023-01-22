ac18b97bda64360a7a09d19ce01b220a756fa77d
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-23 03:41:51 +0530

    query multiple elements

---
+++ src/components/skills/skills.tsx
@@ -0,0 +1,15 @@
+import { SkillsProps } from "./skills.types"
+
+export const Skills = (props: SkillsProps) => {
+  const { skills } = props
+
+  return (
+    <>
+      <ul>
+        {skills.map((skill) => {
+          return <li key={skill}>{skill}</li>
+        })}
+      </ul>
+    </>
+  )
+}
