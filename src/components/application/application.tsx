5af6a9c980f603e9cd115dd2250a2dbfe74279ec
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-23 03:17:56 +0530

    getByLabelText

--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -13,7 +13,7 @@ export const Application = () => {
           <textarea name="bio" id="bio"></textarea>
         </div>
         <div>
-          <label htmlFor="job-location">Job location</label>
+          <label htmlFor="job-location">Name</label>
           <select id="job-location">
             <option value="">Select a country</option>
             <option value="US">United States</option>
