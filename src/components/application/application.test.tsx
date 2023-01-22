8ba65c0bf86bee6c5d10dbc53726340ef35e6b31
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-23 03:30:14 +0530

    getByTestId

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -23,6 +23,9 @@ describe("application", () => {
     const imageElement = screen.getByAltText("a person with a laptop")
     expect(imageElement).toBeInTheDocument()
 
+    const customElement = screen.getByTestId("custom-element")
+    expect(customElement).toBeInTheDocument()
+
     const nameElement = screen.getByRole("textbox", {
       name: "Name",
     })
