7ab3b1562f6a09cbfd4975ba5224afe9c68eef5f
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-23 14:34:45 +0530

    pointer interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css"
-import { Application } from "./components/application/application"
-import { Skills } from "./components/skills/skills"
+import { Counter } from "./components/counter/counter"
 
 function App() {
   return (
     <div className="App">
-      <Application />
-      <Skills skills={["HTML", "CSS", "JavaScript"]} />
+      <Counter />
     </div>
   )
 }
