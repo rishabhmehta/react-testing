93e2a7bfd23cf3f46434ecc834a5be439a4e1f2d
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-23 13:58:11 +0530

    testing playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
+import { Application } from "./components/application/application"
 import { Skills } from "./components/skills/skills"
 
 function App() {
   return (
     <div className="App">
+      <Application />
       <Skills skills={["HTML", "CSS", "JavaScript"]} />
     </div>
   )
