c87209022dc7312e04972011e5ffabac956230a4
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-23 15:02:41 +0530

    custom render functions

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,12 +1,20 @@
-import { render, screen } from "@testing-library/react"
-import { AppProviders } from "../../app-providers"
+import { render, screen } from "../../test-utils"
 import { MuiMode } from "./mui-mode"
 
 describe("MuiMode", () => {
+  /* 
+  GOOD FOR ONE OFF SCENARIOS
   test("renders text correctly", () => {
     render(<MuiMode />, {
       wrapper: AppProviders,
     })
+    const headingElement = screen.getByRole("heading")
+    expect(headingElement).toHaveTextContent("dark mode")
+  })
+  */
+
+  test("renders text correctly", () => {
+    render(<MuiMode />)
     const headingElement = screen.getByRole("heading")
     expect(headingElement).toHaveTextContent("dark mode")
   })
